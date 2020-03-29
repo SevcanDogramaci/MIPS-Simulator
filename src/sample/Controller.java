146727add1914288702db7442d2c165af95b5d02
cataltasz <mustafacataltas@yandex.com>
2020-03-29 17:18:12 +0300

    Comment removal

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,6 +11,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 public class Controller {
 
@@ -30,6 +32,8 @@ public class Controller {
 
         File selectedFile = fileChooser.showOpenDialog(new Stage());
 
+        if (selectedFile == null) return;
+
         // read this file and save into a string.
         try (FileReader reader = new FileReader(selectedFile);
              BufferedReader br = new BufferedReader(reader)) {
@@ -46,6 +50,47 @@ public class Controller {
         } catch (IOException e) {
             System.err.format("IOException: %s%n", e);
         }
+    }
+
+
+    public void clearComments(ArrayList<String> lines){
+
+        boolean commentFlag = false;
+
+        ListIterator<String> iterator
+                = lines.listIterator();
+
+        while (iterator.hasNext()) {
+
+
+            String line = iterator.next();
+            line = line.trim();
+
+            if (commentFlag){  // skips until the end of comment block --> only works for good formatted files.
+
+                if (!line.contains("*/")) {
+                    iterator.remove();
+                    continue;
+                }
+                else commentFlag = false;
+            }
+
+            if (line.startsWith("//")){ // eliminate comment line
+                iterator.remove();
+                continue;
+            }
+
+            if (line.contains("//")){  // eliminate comment part of the line
+                iterator.set(line.substring(0, line.indexOf("//")).trim());
+            }
+
+            if (line.contains("/*")){
+                commentFlag = true;
+
+            }
+
+        }
+
     }
 
 }
