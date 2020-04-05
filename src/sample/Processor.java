7324b7abe2cba21e749cf63e8e75ba69b1c5f5fe
SevcanDogramaci <sevcan.dog@gmail.com>
2020-04-05 18:46:26 +0300

    Processor class is created

---
+++ src/sample/Processor.java
@@ -0,0 +1,82 @@
+package sample;
+
+import java.util.List;
+
+public class Processor {
+
+    private ProgramCounter pc;
+    private RegisterFile registerFile;
+    private InstructionMemoryFile instructionMemoryFile;
+    private MemoryFile memory;
+    private ALU alu;
+
+    public Processor() {
+        pc = new ProgramCounter();
+        registerFile = new RegisterFile();
+        instructionMemoryFile = new InstructionMemoryFile();
+        memory = new MemoryFile(1000);
+        alu = new ALU();
+    }
+
+    public void loadInstructionsToMemory(List<Instruction> instructions) {
+
+        this.instructionMemoryFile.load(instructions);
+        reset();
+    }
+
+    private void reset() {
+
+        pc.reset();
+        registerFile.resetData();
+        memory.resetData();
+    }
+
+    public void step() {
+
+        if(isDone()) {
+            return;
+        }
+
+        Instruction instruction;
+        int alu_out = 0, data_out = 0, regData1 = 0, regData2 = 0,
+            new_pc = pc.get(), branch_pc = new_pc, write_data;
+        boolean alu_zero = false;
+
+        // fetch instruction
+        instruction = instructionMemoryFile.fetch(pc);
+
+        // send instruction to control unit
+        ControlUnit controlUnit = new ControlUnit(instruction);
+
+        // ...
+
+        // update pc 
+        updatePc(instruction, new_pc, branch_pc, alu_zero, controlUnit);
+    }
+
+    private void updatePc(Instruction instruction, int new_pc, int branch_pc, boolean alu_zero, ControlUnit controlUnit) {
+        new_pc += 4;
+
+        if(instruction.isIFormat()){
+            branch_pc = new_pc + ((IFormatInstruction)instruction).getImmediate() << 2;
+        }
+        else if(instruction.isJFormat()){
+            branch_pc = (int) (new_pc + ((JFormatInstruction)instruction).getTargetOffset() << 2);
+        }
+
+        // update pc if branching exists
+        new_pc = mux(new_pc, branch_pc, controlUnit.isBranch() && alu_zero);
+        pc.set(new_pc);
+    }
+
+    private int mux(int value1, int value2, boolean getSecond) {
+        if(getSecond) {
+            return value2;
+        }
+        return value1;
+    }
+
+    private boolean isDone() {
+        return pc.get() >= instructionMemoryFile.length();
+    }
+}
