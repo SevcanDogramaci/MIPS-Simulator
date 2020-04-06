193863978c699fb9474ac9c33c3ae2818bde2ea8
SevcanDogramaci <sevcan.dog@gmail.com>
2020-04-06 14:33:05 +0300

    Shiftamount and immediate are added

--- src/sample/Instruction.java
+++ src/sample/Instruction.java
@@ -3,8 +3,8 @@ package sample;
 public abstract class Instruction {
 
     protected short opcode;
-    protected int index;
-    protected Register sourceReg1, sourceReg2, destinationReg;
+    protected int index, shiftAmount, immediate;
+    protected Register sourceReg, targetReg, destinationReg;
 
     public static Instruction createInstruction(String line, int i, Parser parser) throws Exception {
 
@@ -23,16 +23,28 @@ public abstract class Instruction {
     abstract void parseInstruction (String line) throws Exception;
 
     public boolean isRegS1 () {
-        return sourceReg1 != null;
+        return sourceReg != null;
     }
 
     public boolean isRegS2 () {
-        return sourceReg1 != null;
+        return sourceReg != null;
     }
 
     public boolean isRegDest () {
         return destinationReg != null;
     }
+
+    public Register getSourceReg() { return sourceReg; }
+
+    public Register getTargetReg() { return targetReg; }
+
+    public Register getDestinationReg() { return destinationReg; }
+
+    public short getFunction() { return opcode; };
+
+    public int getShiftAmount() { return shiftAmount; }
+
+    public int getImmediate() { return immediate; }
 
     public boolean isRFormat(){
         return this instanceof RFormatInstruction;
@@ -45,4 +57,5 @@ public abstract class Instruction {
     public boolean isJFormat(){
         return this instanceof JFormatInstruction;
     }
+
 }
