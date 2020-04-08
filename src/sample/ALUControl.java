bc72d0a6fd257950408ebb6e3f9aecfe7e15588b
SevcanDogramaci <sevcan.dog@gmail.com>
2020-04-08 12:55:32 +0300

    Switch case update

--- src/sample/ALUControl.java
+++ src/sample/ALUControl.java
@@ -15,18 +15,30 @@ public class ALUControl {
 
         switch(funct & 15) {
             case 0:
+            case 1:
+                System.out.println("ADD");
                 return ALU.ADD;
+
             case 2:
+                System.out.println("SUB");
                 return ALU.SUBTRACT;
+
+            case 4:
             case 12:
                 System.out.println("AND");
                 return ALU.AND;
+
+            case 5:
             case 11:
                 System.out.println("OR");
                 return ALU.OR;
+
             case 7:
+                System.out.println("NOR");
                 return ALU.NOR;
+
             case 10:
+                System.out.println("SLT");
                 return ALU.SLT;
         }
 
