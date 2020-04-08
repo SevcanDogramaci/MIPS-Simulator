3738d61a6c096fa2f7bcb58cbfb56820c54c3adb
SevcanDogramaci <sevcan.dog@gmail.com>
2020-04-08 16:22:18 +0300

    Comments are added

--- src/sample/IFormatInstruction.java
+++ src/sample/IFormatInstruction.java
@@ -91,24 +91,29 @@ public class IFormatInstruction extends Instruction {
         instructionMap.put("addi", (short)8);   // +
         instructionMap.put("addiu", (short)9);  // +
         instructionMap.put("andi", (short)12);  // +
-        instructionMap.put("beq", (short)4);
+
+        instructionMap.put("beq", (short)4);    // +
         instructionMap.put("bgez", (short)1);   // //
         instructionMap.put("bgtz", (short)7);
         instructionMap.put("blez", (short)6);
         instructionMap.put("bltz", (short)1);   // //
-        instructionMap.put("bne", (short)5);
+        instructionMap.put("bne", (short)5);    // +
+
         instructionMap.put("lb", (short)32);
         instructionMap.put("lbu", (short)33);
         instructionMap.put("lhu", (short)37);
         instructionMap.put("lui", (short)15);
         instructionMap.put("lw", (short)35);
         instructionMap.put("lwcl", (short)49);
+
         instructionMap.put("ori", (short)11);   // +
+
         instructionMap.put("sb", (short)40);
         instructionMap.put("slti", (short)10);  // +
         instructionMap.put("sh", (short)41);
         instructionMap.put("sw", (short)43);
         instructionMap.put("swcl", (short)57);
+
         instructionMap.put("xori", (short)14);
     }
 
--- src/sample/RFormatInstruction.java
+++ src/sample/RFormatInstruction.java
@@ -100,9 +100,9 @@ public class RFormatInstruction extends Instruction {
         instructionMap = new HashMap<>();
         // put instructions;
 
-        instructionMap.put("add", "100000 1110");
-        instructionMap.put("addu", "100001 1110");
-        instructionMap.put("and", "100100 1110");
+        instructionMap.put("add", "100000 1110");   // +
+        instructionMap.put("addu", "100001 1110");  // +
+        instructionMap.put("and", "100100 1110");   // +
         instructionMap.put("break", "00001101 0000");
         instructionMap.put("div", "011010 0110");
         instructionMap.put("divu", "011011 0110");
@@ -116,19 +116,19 @@ public class RFormatInstruction extends Instruction {
         instructionMap.put("mult", "011000 0110");
         instructionMap.put("multu", "011001 0110");
         instructionMap.put("nor", "100111 1110");
-        instructionMap.put("or", "100101 1110");
+        instructionMap.put("or", "100101 1110");    // +
 
         instructionMap.put("sll", "000000 1011");
         instructionMap.put("sllv", "000100 1110");
-        instructionMap.put("slt", "101010 1110");
-        instructionMap.put("sltu", "101011 1110");
+        instructionMap.put("slt", "101010 1110");   // +
+        instructionMap.put("sltu", "101011 1110");  // büyük ihtimal olacak ama case çakışmasından dolayı eklenmedi ALUControl'e
         instructionMap.put("sra", "000011 1011");
         instructionMap.put("srav", "000111 1110");
         instructionMap.put("srl", "000010 1011");
         instructionMap.put("srlv", "000110 1110");
 
-        instructionMap.put("sub", "100010 1110");
-        instructionMap.put("subu", "100011 1110");
+        instructionMap.put("sub", "100010 1110");   // +
+        instructionMap.put("subu", "100011 1110");  // +
         instructionMap.put("syscall", "001100 0000");
         instructionMap.put("xor", "100110 1110");
     }
