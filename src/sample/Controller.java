cc55fdc3936d25ed312aa613b515776277f7a5d6
cataltasz <mustafacataltas@yandex.com>
2020-04-17 21:01:57 +0300

    Restart and run pressed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -148,6 +148,7 @@ public class Controller {
     @FXML
     public void restartApplication() {
         assemblyCodeArea.setText("");
+        assemblyCodeArea.setEditable(true);
         btnRun.setDisable(false);
         btnStep.setDisable(true);
         btnChoose.setDisable(false);
