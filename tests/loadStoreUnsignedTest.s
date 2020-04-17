a3428d5e944d92472a3a7e8de69218a151b5a6a0
SevcanDogramaci <sevcan.dog@gmail.com>
2020-04-17 22:48:42 +0300

    Test cases are added and updated

--- tests/loadStoreUnsignedTest.s
+++ tests/loadStoreUnsignedTest.s
@@ -1,14 +1,10 @@
 foo:
-addi $a0, $a0, 17
 addi $sp, $sp, -32
+sw $a0, 16($sp)
+sw $ra, 12($sp)
+sw $fp, 8($sp)
+addiu $fp, $sp, 28
+addi $a0, $a0, 128
 sb $a0, 16($sp)
-addi $a0, $a0, 1
-sh $a0, 12($sp)
-addi $a0, $a0, 1
-sw $a0, 8($sp)
-lw $a1, 8($sp)
-lh $a2, 12($sp)
-lb $a3, 16($sp)
-addiu $sp, $sp, 32
-
-exit:
+lbu $a1, 16($sp)
+lhu $a2, 16($sp)
