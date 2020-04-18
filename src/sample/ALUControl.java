8742b5358dac0cd7d4cdc7765760cef2e6ccf40c
SevcanDogramaci <sevcan.dog@gmail.com>
2020-04-18 14:19:49 +0300

    Switch case is updated with new function cases

--- src/sample/ALUControl.java
+++ src/sample/ALUControl.java
@@ -5,42 +5,60 @@ public class ALUControl {
     public static short getControl(boolean ALUOp1, boolean ALUOp0, short funct) {
 
         if(!ALUOp1 && !ALUOp0) {
-            return ALU.ADD;
+                System.out.println("ADD");
+                return ALU.ADD;
         }
         if(ALUOp0) {
+            System.out.println("SUB");
             return ALU.SUBTRACT;
         }
 
         System.out.println("result = " + (funct & 15));
 
-        switch(funct & 15) {
-            case 0:
-            case 1:
+        switch(funct & 63) {
+            case 32:
+            case 33:
                 System.out.println("ADD");
                 return ALU.ADD;
 
-            case 2:
-            case 3:
+            case 34:
+            case 35:
                 System.out.println("SUB");
                 return ALU.SUBTRACT;
 
-            case 4:
+            case 36:
             case 12:
                 System.out.println("AND");
                 return ALU.AND;
 
-            case 5:
+            case 37:
             case 11:
                 System.out.println("OR");
                 return ALU.OR;
 
-            case 7:
+            case 39:
                 System.out.println("NOR");
                 return ALU.NOR;
 
-            case 10:
+            case 42:
+            case 43:
                 System.out.println("SLT");
                 return ALU.SLT;
+
+            case 0:
+            case 4:
+                System.out.println("SLL");
+                return ALU.SLL;
+
+            case 2:
+            case 6:
+                System.out.println("SRL");
+                return ALU.SRL;
+
+            case 3:
+            case 7:
+                System.out.println("SRA");
+                return ALU.SRA;
         }
 
         System.out.println("NO RESULT");
