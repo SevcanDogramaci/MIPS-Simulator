15a42e3b88cd073f1fee3ca0f3ac9e6cf93e2dbc
SevcanDogramaci <sevcan.dog@gmail.com>
2020-05-12 14:47:56 +0300

    GUI methods modifications and code cleanup are made

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,11 @@
 package sample;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.collections.ObservableList;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+import javafx.event.EventHandler;
 import javafx.stage.FileChooser;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
@@ -24,22 +20,18 @@ import java.io.File;
 public class Controller {
 
     @FXML private Rectangle rectangle;
+    @FXML private TextArea assemblyCodeArea, lineNum;
     @FXML private Button btnRun, btnStep, btnChoose, btnReset;
-    @FXML private TextArea assemblyCodeArea;
-    @FXML private Label lblLine;
     @FXML private TableView<Register> rTable;
-    @FXML private TableColumn<Register, Integer> rNo, rValue;
     @FXML private TableColumn<Register, String > rName;
-    @FXML private TableColumn<Data, String > textSegAddress, textSegValue, sAddress, sValue;
+    @FXML private TableColumn<Register, Integer> rNo, rValue;
     @FXML private TableView<Data> textSegTable, sTable;
+    @FXML private TableColumn<Data, String > textSegAddress, textSegValue, sAddress, sValue;
 
-    @FXML private TextArea lineNum;
-
-    //@FXML private ScrollPane lineScroll, codeScroll;
-
+    private final short TOOLTIP_DELAY = 10;
     private Parser parser;
     private Processor processor;
-    private double rectStartingY;
+
 
     @FXML
     private void chooseRect(){ assemblyCodeArea.requestFocus(); }
@@ -59,8 +51,14 @@ public class Controller {
     }
 
     private void setRectY(int count){
-        //rectangle.setTranslateY(count * 19);
-        if(count < 28) rectangle.setY(rectStartingY + count*rectangle.getHeight());
+        int scrollValue = (int)assemblyCodeArea.getScrollTop() ;
+        if(count < 27)
+            rectangle.setTranslateY(count * 19 - scrollValue);
+        else
+            rectangle.setTranslateY((count-1) * 19 - scrollValue);
+
+        // if(count < 27) rectangle.setTranslateY(count * 19);
+        // if(count < 28) rectangle.setY(rectStartingY + count*rectangle.getHeight());
     }
 
     private void setLineNumber(String text){
@@ -71,52 +69,42 @@ public class Controller {
         {    if(text.charAt(i) == '\n')
                 sb.append("\n").append(++count);
         }
-
-        //lblLine.setText(sb.toString());
         lineNum.setText(sb.toString());
     }
 
     @FXML
     public void initialize(){
 
-        lineNum.scrollTopProperty().bindBidirectional(assemblyCodeArea.scrollTopProperty());
-        assemblyCodeArea.setOnKeyReleased(new EventHandler<KeyEvent>()
-        {
-            @Override
-            public void handle(KeyEvent keyEvent)
-            {
-                if(keyEvent.getCode() == KeyCode.ENTER)
-                {
-                    assemblyCodeArea.setScrollTop(Double.MAX_VALUE);
-                    lineNum.setScrollTop(Double.MAX_VALUE);
-                }
-            }
-        });
-
         setUpAssemblyCodeAreas();
         setUpTablePlaceholders();
         setupRegisterTable();
 
-        Tooltip runTip = new Tooltip("Run");
-        runTip.setShowDelay(Duration.millis(10));
-        Tooltip.install(btnRun, runTip);
+        createButtonTooltip("Run", TOOLTIP_DELAY, btnRun);
+        createButtonTooltip("Choose File", TOOLTIP_DELAY, btnChoose);
+        createButtonTooltip("Reset", TOOLTIP_DELAY, btnReset);
+        createButtonTooltip("Step", TOOLTIP_DELAY, btnStep);
+    }
 
-        Tooltip chooseTip = new Tooltip("Choose File");
-        chooseTip.setShowDelay(Duration.millis(10));
-        Tooltip.install(btnChoose, chooseTip);
-
-        Tooltip resetTip = new Tooltip("Reset");
-        resetTip.setShowDelay(Duration.millis(10));
-        Tooltip.install(btnReset, resetTip);
-
-        Tooltip stepTip = new Tooltip("Step");
-        stepTip.setShowDelay(Duration.millis(10));
-        Tooltip.install(btnStep, stepTip);
+    private void createButtonTooltip(String tooltip, short delay, Button button) {
+        Tooltip runTip = new Tooltip(tooltip);
+        runTip.setShowDelay(Duration.millis(delay));
+        Tooltip.install(button, runTip);
     }
 
     private void setUpAssemblyCodeAreas() {
+
+        lineNum.scrollTopProperty().bindBidirectional(assemblyCodeArea.scrollTopProperty());
+        assemblyCodeArea.setOnKeyReleased(new EventHandler<>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER) {
+                    //assemblyCodeArea.setScrollTop(Double.MAX_VALUE);
+                    //lineNum.setScrollTop(Double.MAX_VALUE);
+                }
+            }
+        });
+        rectangle.setOnMouseClicked(mouseEvent -> chooseRect());
         assemblyCodeArea.addEventHandler(KeyEvent.KEY_RELEASED, keyEvent -> handleRect());
-        rectStartingY = rectangle.getY();
         assemblyCodeArea.textProperty().addListener((observableValue, s, t1) -> setLineNumber(t1));
         rectangle.widthProperty().bind(assemblyCodeArea.widthProperty().
                 add(lineNum.widthProperty()).
@@ -134,13 +122,12 @@ public class Controller {
         rName.setCellValueFactory(new PropertyValueFactory<>("name"));
         rTable.setItems(RegisterFile.getRegisters());
         rTable.getItems().forEach(
-                register -> register.addListener(
-                        (observableValue, o, t1) -> {
-            int regNo =((Register)observableValue).getNo();
-            rTable.getSelectionModel().select(regNo);
-            if (regNo > 3)
-                rTable.scrollTo(regNo - 4);
-        }));
+                register -> register.addListener((observableValue, o, t1) -> {
+                    int regNo =((Register)observableValue).getNo();
+                    rTable.getSelectionModel().select(regNo);
+                    if (regNo > 3)
+                        rTable.scrollTo(regNo - 4);
+                }));
     }
 
     private void setupTextSegmentTable() throws Exception {
@@ -179,9 +166,7 @@ public class Controller {
         try{
             parser.createInstructions();
             assemblyCodeArea.setText(parser.getLines());
-
             List<Instruction> instructions = parser.getInstructions();
-
             processor = new Processor();
             processor.loadInstructionsToMemory(instructions);
             setupTextSegmentTable();
@@ -190,7 +175,6 @@ public class Controller {
         } catch (Exception e){
             showAlertDialog("Error", e.getMessage(), true);
         }
-
     }
 
     private void startAgain () throws Exception {
@@ -207,19 +191,21 @@ public class Controller {
     public void onStep(ActionEvent event) throws Exception {
         try {
             if (!processor.isDone()) {
+                handleRect();
                 processor.step();
                 rTable.refresh();
                 setupStackTable(processor.getStackData());
                 selectLine(processor.getIndex());
             } else {
-                setRectY(0);
+                //setRectY(0);
+                //assemblyCodeArea.selectRange(0,0);
                 showAlertDialog("The program has finished!", "Do you want to run again ?", false);
             }
         } catch (Exception e){
             showAlertDialog("Problem at line " + (processor.getIndex() + 1), e.getMessage(), false);
         }
     }
-/*
+
     private int ordinalIndexOf(String str, String substr, int n) {
         int pos = -1;
         do {
@@ -227,19 +213,21 @@ public class Controller {
         } while (n-- > 0 && pos != -1);
         return pos;
     }
-*/
-    private void selectLine(int lineNum){
-        textSegTable.getSelectionModel().select(lineNum);
-        if(lineNum > 3)
-            textSegTable.scrollTo(lineNum - 4);
-        setRectY(lineNum);
 
-        /*String txt = assemblyCodeArea.getText();
+    private void selectLine(int lineCount){
+        textSegTable.getSelectionModel().select(lineCount);
+        if(lineCount > 3)
+            textSegTable.scrollTo(lineCount - 4);
+        setRectY(lineCount);
 
-        int start = lineNum == 0 ? 0 : ordinalIndexOf(txt, "\n", lineNum - 1);
-        int end = ordinalIndexOf(txt, "\n", lineNum );
+        /*
+        String txt = assemblyCodeArea.getText();
 
-        assemblyCodeArea.selectRange(start, end);*/
+        int start = lineCount == 0 ? 0 : ordinalIndexOf(txt, "\n", lineCount - 1);
+        int end = ordinalIndexOf(txt, "\n", lineCount );
+
+        assemblyCodeArea.selectRange(start, end);
+        */
     }
 
     @FXML
@@ -256,6 +244,8 @@ public class Controller {
         File selectedFile = fileChooser.showOpenDialog(new Stage());
 
         if (selectedFile == null) return;
+
+        resetApplication();
 
         assemblyCodeArea.setEditable(false);
 
